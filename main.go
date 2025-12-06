78605eb8e374da1d96d30a4934d8fdefda16503d
S42yt <songoku42@outlook.de>
2025-12-06 20:16:50 +0300

    add setaccentcolor

--- main.go
+++ main.go
@@ -1,132 +1,138 @@
 package v2
 
 import (
-    "encoding/json"
-    "github.com/bwmarrin/discordgo"
+	"encoding/json"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 const (
-    ComponentTypeContainer    discordgo.ComponentType = 17
-    ComponentTypeMediaGallery discordgo.ComponentType = 12
-    ComponentTypeTextDisplay  discordgo.ComponentType = 10
-    ComponentTypeSection      discordgo.ComponentType = 9
+	ComponentTypeContainer    discordgo.ComponentType = 17
+	ComponentTypeMediaGallery discordgo.ComponentType = 12
+	ComponentTypeTextDisplay  discordgo.ComponentType = 10
+	ComponentTypeSection      discordgo.ComponentType = 9
 )
 
 type TextDisplay struct {
-    Content string `json:"content"`
+	Content string `json:"content"`
 }
 
 func (t TextDisplay) Type() discordgo.ComponentType {
-    return ComponentTypeTextDisplay
+	return ComponentTypeTextDisplay
 }
 
 func (t TextDisplay) MarshalJSON() ([]byte, error) {
-    type textDisplay TextDisplay
-    return json.Marshal(struct {
-        textDisplay
-        Type discordgo.ComponentType `json:"type"`
-    }{
-        textDisplay: textDisplay(t),
-        Type:        t.Type(),
-    })
+	type textDisplay TextDisplay
+	return json.Marshal(struct {
+		textDisplay
+		Type discordgo.ComponentType `json:"type"`
+	}{
+		textDisplay: textDisplay(t),
+		Type:        t.Type(),
+	})
 }
 
 func NewTextDisplayBuilder() *TextDisplay {
-    return &TextDisplay{}
+	return &TextDisplay{}
 }
 
 func (t *TextDisplay) SetContent(content string) *TextDisplay {
-    t.Content = content
-    return t
+	t.Content = content
+	return t
 }
 
 func (t *TextDisplay) Build() discordgo.MessageComponent {
-    return *t
+	return *t
 }
 
 type MediaGallery struct {
-    ID    int                  `json:"id,omitempty"`
-    Items []MediaGalleryItem   `json:"items"`
+	ID    int                `json:"id,omitempty"`
+	Items []MediaGalleryItem `json:"items"`
 }
 
 type MediaGalleryItem struct {
-    Media       UnfurledMediaItem `json:"media"`
-    Description *string           `json:"description,omitempty"`
-    Spoiler     bool              `json:"spoiler"`
+	Media       UnfurledMediaItem `json:"media"`
+	Description *string           `json:"description,omitempty"`
+	Spoiler     bool              `json:"spoiler"`
 }
 
 type UnfurledMediaItem struct {
-    URL string `json:"url"`
+	URL string `json:"url"`
 }
 
 func (m MediaGallery) Type() discordgo.ComponentType {
-    return ComponentTypeMediaGallery
+	return ComponentTypeMediaGallery
 }
 
 func (m MediaGallery) MarshalJSON() ([]byte, error) {
-    type mediaGallery MediaGallery
-    return json.Marshal(struct {
-        mediaGallery
-        Type discordgo.ComponentType `json:"type"`
-    }{
-        mediaGallery: mediaGallery(m),
-        Type:         m.Type(),
-    })
+	type mediaGallery MediaGallery
+	return json.Marshal(struct {
+		mediaGallery
+		Type discordgo.ComponentType `json:"type"`
+	}{
+		mediaGallery: mediaGallery(m),
+		Type:         m.Type(),
+	})
 }
 
 func NewMediaGalleryBuilder() *MediaGallery {
-    return &MediaGallery{
-        Items: []MediaGalleryItem{},
-    }
+	return &MediaGallery{
+		Items: []MediaGalleryItem{},
+	}
 }
 
 func (m *MediaGallery) AddImageURL(url string) *MediaGallery {
-    m.Items = append(m.Items, MediaGalleryItem{
-        Media: UnfurledMediaItem{
-            URL: url,
-        },
-        Spoiler: false,
-    })
-    return m
+	m.Items = append(m.Items, MediaGalleryItem{
+		Media: UnfurledMediaItem{
+			URL: url,
+		},
+		Spoiler: false,
+	})
+	return m
 }
 
 func (m *MediaGallery) Build() discordgo.MessageComponent {
-    return *m
+	return *m
 }
 
 type Container struct {
-    ID          int                  `json:"id,omitempty"`
-    AccentColor *int                 `json:"accent_color,omitempty"`
-    Spoiler     bool                 `json:"spoiler"`
-    Components  []discordgo.MessageComponent `json:"components"`
+	ID          int                          `json:"id,omitempty"`
+	AccentColor *int                         `json:"accent_color,omitempty"`
+	Spoiler     bool                         `json:"spoiler"`
+	Components  []discordgo.MessageComponent `json:"components"`
 }
 
 func (c Container) Type() discordgo.ComponentType {
-    return ComponentTypeContainer
+	return ComponentTypeContainer
 }
 
 func (c Container) MarshalJSON() ([]byte, error) {
-    type container Container
-    return json.Marshal(struct {
-        container
-        Type discordgo.ComponentType `json:"type"`
-    }{
-        container: container(c),
-        Type:      c.Type(),
-    })
+	type container Container
+	return json.Marshal(struct {
+		container
+		Type discordgo.ComponentType `json:"type"`
+	}{
+		container: container(c),
+		Type:      c.Type(),
+	})
 }
 
 func NewContainerBuilder() *Container {
-    return &Container{
-        Components: []discordgo.MessageComponent{},
-    }
+	return &Container{
+		Components: []discordgo.MessageComponent{},
+	}
 }
 
 func (c *Container) AddComponent(comp discordgo.MessageComponent) *Container {
-    c.Components = append(c.Components, comp)
-    return c
+	c.Components = append(c.Components, comp)
+	return c
+}
+
+func (c *Container) SetAccentColor(color int) *Container {
+	c.AccentColor = &color
+	return c
 }
 
 func (c *Container) Build() discordgo.MessageComponent {
-    return *c
+	return *c
 }
