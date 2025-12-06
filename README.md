0aa04a30a9a185ea6d6b94492476c528f23f088f
S42yt <songoku42@outlook.de>
2025-12-06 20:04:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DiscordGo Components v2
+This is a simple library to add support for Discord Components v2 for DiscordGo
+Have fun while coding :P !
---
+++ main.go
@@ -0,0 +1,132 @@
+package v2
+
+import (
+    "encoding/json"
+    "github.com/bwmarrin/discordgo"
+)
+
+const (
+    ComponentTypeContainer    discordgo.ComponentType = 17
+    ComponentTypeMediaGallery discordgo.ComponentType = 12
+    ComponentTypeTextDisplay  discordgo.ComponentType = 10
+    ComponentTypeSection      discordgo.ComponentType = 9
+)
+
+type TextDisplay struct {
+    Content string `json:"content"`
+}
+
+func (t TextDisplay) Type() discordgo.ComponentType {
+    return ComponentTypeTextDisplay
+}
+
+func (t TextDisplay) MarshalJSON() ([]byte, error) {
+    type textDisplay TextDisplay
+    return json.Marshal(struct {
+        textDisplay
+        Type discordgo.ComponentType `json:"type"`
+    }{
+        textDisplay: textDisplay(t),
+        Type:        t.Type(),
+    })
+}
+
+func NewTextDisplayBuilder() *TextDisplay {
+    return &TextDisplay{}
+}
+
+func (t *TextDisplay) SetContent(content string) *TextDisplay {
+    t.Content = content
+    return t
+}
+
+func (t *TextDisplay) Build() discordgo.MessageComponent {
+    return *t
+}
+
+type MediaGallery struct {
+    ID    int                  `json:"id,omitempty"`
+    Items []MediaGalleryItem   `json:"items"`
+}
+
+type MediaGalleryItem struct {
+    Media       UnfurledMediaItem `json:"media"`
+    Description *string           `json:"description,omitempty"`
+    Spoiler     bool              `json:"spoiler"`
+}
+
+type UnfurledMediaItem struct {
+    URL string `json:"url"`
+}
+
+func (m MediaGallery) Type() discordgo.ComponentType {
+    return ComponentTypeMediaGallery
+}
+
+func (m MediaGallery) MarshalJSON() ([]byte, error) {
+    type mediaGallery MediaGallery
+    return json.Marshal(struct {
+        mediaGallery
+        Type discordgo.ComponentType `json:"type"`
+    }{
+        mediaGallery: mediaGallery(m),
+        Type:         m.Type(),
+    })
+}
+
+func NewMediaGalleryBuilder() *MediaGallery {
+    return &MediaGallery{
+        Items: []MediaGalleryItem{},
+    }
+}
+
+func (m *MediaGallery) AddImageURL(url string) *MediaGallery {
+    m.Items = append(m.Items, MediaGalleryItem{
+        Media: UnfurledMediaItem{
+            URL: url,
+        },
+        Spoiler: false,
+    })
+    return m
+}
+
+func (m *MediaGallery) Build() discordgo.MessageComponent {
+    return *m
+}
+
+type Container struct {
+    ID          int                  `json:"id,omitempty"`
+    AccentColor *int                 `json:"accent_color,omitempty"`
+    Spoiler     bool                 `json:"spoiler"`
+    Components  []discordgo.MessageComponent `json:"components"`
+}
+
+func (c Container) Type() discordgo.ComponentType {
+    return ComponentTypeContainer
+}
+
+func (c Container) MarshalJSON() ([]byte, error) {
+    type container Container
+    return json.Marshal(struct {
+        container
+        Type discordgo.ComponentType `json:"type"`
+    }{
+        container: container(c),
+        Type:      c.Type(),
+    })
+}
+
+func NewContainerBuilder() *Container {
+    return &Container{
+        Components: []discordgo.MessageComponent{},
+    }
+}
+
+func (c *Container) AddComponent(comp discordgo.MessageComponent) *Container {
+    c.Components = append(c.Components, comp)
+    return c
+}
+
+func (c *Container) Build() discordgo.MessageComponent {
+    return *c
+}
